6951bf94fc60e82d0076f0528dabd0e46c2d732d
Tim Whittley <twhittley@freshbooks.com>
2016-04-22 17:08:00 -0400

    controllers partially done

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  songCreationStarted: false,
   actions: {
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
+    },
+    canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+        return this.get('songCreationStarted') || this.get('model.songs.length');
+      }),
     updateRating: function(params) {
       var song = params.item,
           rating = params.rating;
